23d9682931bd83324e2ccac7361958efb3b761be
LLT21 <43903768+LLT21@users.noreply.github.com>
2021-04-26 21:15:43 +0200

    first commit

---
+++ ConnectionStrings.cs
@@ -0,0 +1,8 @@
+namespace nativesql
+{
+   public static class ConnectionStrings
+   {
+      public const string OdbcConnection = "Driver={ODBC Driver 17 for SQL Server};Server=;Database=;Uid=;Pwd=";
+      public const string SqlConnection = "server=;user id=;password=;Initial Catalog=;Connection Timeout=120;";
+   }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Data;
+using System.Data.Odbc;
+using Microsoft.Data.SqlClient;
+
+namespace nativesql
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+
+         //Odbc is working in Native compilation mode
+         //Using: dotnet publish -r osx.11.0-x64 -c release
+         //Then execute: /bin/release/net5.0/osx.11.0-x64/native/nativesql
+
+         using (IDbConnection dbConnection = new OdbcConnection(ConnectionStrings.OdbcConnection))
+         {
+            string names = string.Empty;
+
+            using (IDbCommand dbCommand = dbConnection.CreateCommand())
+            {
+               dbCommand.CommandText = "SELECT * FROM Contact";
+               dbCommand.CommandType = CommandType.Text;
+
+               dbConnection.Open();
+
+               using (IDataReader dataReader = dbCommand.ExecuteReader())
+               {
+                  while (dataReader.Read())
+                  {
+                     names += ", " + Convert.ToString(dataReader["Name1"]);
+                  }
+               }
+
+               Console.WriteLine(names);
+            }
+         }
+
+         //SqlClient is not working in Native compilation mode: stops at the using step
+
+         using (IDbConnection dbConnection = new SqlConnection(ConnectionStrings.SqlConnection))
+         {
+            string names = string.Empty;
+
+            using (IDbCommand dbCommand = dbConnection.CreateCommand())
+            {
+               dbCommand.CommandText = "SELECT * FROM Contact";
+               dbCommand.CommandType = CommandType.Text;
+
+               dbConnection.Open();
+
+               using (IDataReader dataReader = dbCommand.ExecuteReader())
+               {
+                  while (dataReader.Read())
+                  {
+                     names += ", " + Convert.ToString(dataReader["Name1"]);
+                  }
+               }
+            }
+
+            Console.WriteLine(names);
+         }
+      }
+   }
+}
